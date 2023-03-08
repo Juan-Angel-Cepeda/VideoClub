46c7d633d7e9c2d87ca334ebfcfb600f10ed27fe
Angel Cepeda <a338832@uach.mx>
2023-03-08 09:01:01 -0700

    New functios for controllers/models

--- controllers/directors.js
+++ controllers/directors.js
@@ -11,7 +11,7 @@ function index(req, res, next){
     const id = req.params.id;
     Director.findByPk(id)
             .then(object => res.json(object))
-            .catch(err => res.json(err));
+            .catch(err => res.send(err));
 };
 
 function create(req, res, next){
@@ -25,19 +25,40 @@ function create(req, res, next){
 
     Director.create(director)
             .then(obj => res.json(obj))
-            .catch(err => res.json(err));
+            .catch(err => res.send(err));
 };
 
 function replace(req, res, next){
-    res.send(`respond with a replace =${req.params.id}`);  
-};
+    const id = req.params.id;
+    Director.findByPk(id)
+            .then((object)=>{
+                const name = req.body.name ? req.body.name :"";
+                const lastName = req.body.lastName ? req.body.lastNamename :"";
+                object.update({name:name,lastName:lastName})
+                .then(obj => res.json(obj))
+                .catch(err => res.send(err));
+            })
+            .catch(err => res.send(err));
+    }
 
 function update(req, res, next){
-    res.send(`respond with an update =${req.params.id}`);  
+    const id = req.params.id;
+    Director.findByPk(id)
+            .then((object)=>{
+                const name = req.body.name ? req.body.name :object.name;
+                const lastName = req.body.lastName ? req.body.lastNamename: object.lastName;
+                object.update({name:name,lastName:lastName})
+                .then(obj => res.json(obj))
+                .catch(err => res.send(err));
+            })
+            .catch(err => res.send(err));
 };
 
 function destroy(req, res, next){
-    res.send(`respond with a destroy =${req.params.id}`);  
+    const id = req.params.id;
+    Director.destroy({where:{id:id}})
+            .then(obj => res.json(obj))
+            .catch(err => res.send(err));
 };
 
 module.exports = {list,index,create,update,destroy,replace};
