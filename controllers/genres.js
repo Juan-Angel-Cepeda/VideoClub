37e2417aa387c2428f79ae5b5445303fd2ee5041
angelcepeda <jangelcepeda@protonmail.com>
2023-04-30 13:55:27 -0700

    genre controller finished

--- controllers/genres.js
+++ controllers/genres.js
@@ -12,23 +12,79 @@ function list(req, res, next){
 };
 
 function index(req, res, next){
-    res.send(`respond with a index = ${req.params.id}`);  
+    const id = req.params.id;
+    Genre.findOne({"_id":id}).then(obj=>res.status(200).json({
+        message: `Genre with id ${id}`,
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No info",
+        err:ex
+    }))
 };
 
 function create(req, res, next){
-    res.send(`respond with a a created name = ${name} and lastname = ${lastname}`);  
+    let description = req.params.description
+    let genre = new Genre({
+        description:description
+    })
+    genre.save().then(obj => res.status(200).json({
+        message:"Generp creado correctamente",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No se pudo almacenar el genero",
+        obj:ex
+    }));
+
 };
 
 function replace(req, res, next){
-    res.send(`respond with a replace =${req.params.id}`);  
+    const id = req.params.id;
+    let description = req.body.description ? req.body.description:"";
+    let genre = new Object({
+        _description:description
+    });
+    Genre.findOneAndUpdate({"_id":id},genre,{new : true})
+    .then(obj => {res.status(200).json({
+        message: "Genero actualizado correctamente",
+        obj:obj
+    })}).catch(ex => res.status(500).json({
+        message:"No se pudo remplazar el genero",
+        obj:ex
+    }));
+
 };
 
 function update(req, res, next){
-    res.send(`respond with an update =${req.params.id}`);  
+    const id = req.params.id
+    let description = req.params.description;
+    
+    let genre = new Object();
+
+    if(description){
+        genre._description = description;
+    }
+
+    Genre.findOneAndUpdate({"_id":id},genre)
+    .then(obj => res.status(200).json({
+        message:"Genero actuaizado correctamente",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No se pudo remplazar el genero",
+        obj:ex
+    }));
+
 };
 
 function destroy(req, res, next){
-    res.send(`respond with a destroy =${req.params.id}`);  
+    const id = req.params.id;
+    Genre.findByIdAndRemove({"_id":id}).then(obj => res.status(200).json({
+        message: "Genero eliminado correctamente",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No se pudo eliminar el genero",
+        obj:ex
+    }));
+    
 };
 
 module.exports = {list,index,create,update,destroy,replace};
